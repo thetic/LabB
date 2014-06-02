de041f350ee3e3953f6842ddfb732a2de0405a0c
thetic <thetic@autistici.org>
2014-06-02 10:08:58 -0700

    style

--- instruction_register.v
+++ instruction_register.v
@@ -5,12 +5,12 @@
  * Instruction register
  */
 module instruction_register #(
-  parameter N = 16        // default bitwidth of register
+  parameter N = 16         // default bitwidth of register
 )(
-  input              clk, // clock
-  input      [N-1:0] d  , // input data
-  output reg [N-1:0] q  , // output data
-  input              ir_ld //load instruction signal
+  input              clk  , // clock
+  input      [N-1:0] d    , // input data
+  output reg [N-1:0] q    , // output data
+  input              ir_ld  //load instruction signal
 );
 
   always @(posedge clk)
