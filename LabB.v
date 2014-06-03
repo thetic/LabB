21a266c5cea097e58d3e98f488fc1fd475a9b57a
aaronst <aaronst@uw.edu>
2014-06-02 17:55:09 -0700

    changed Pc out from 8 to 5 bits

--- LabB.v
+++ LabB.v
@@ -12,7 +12,7 @@ module LabB(KEY, SW, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, HEX6, HEX7);
   wire [15:0] ALU_B               ;
   wire [15:0] ALU_Out             ;
   wire [15:0] RQ                 ;
-  wire [7 :0] PC_Out              ;
+  wire [4 :0] PC_Out              ;
   wire [3 :0] StateO              ;
   wire [15:0] Mux_out             ;
   reg  [15:0] HexMux              ;
