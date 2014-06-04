124338890954338b28610aba5570ec3c7282397f
thetic <thetic@autistici.org>
2014-06-03 23:23:34 -0700

    formatting

--- RegisterOEN.v
+++ RegisterOEN.v
@@ -5,16 +5,17 @@
 module RegisterOEN( Clk, Rst, Ld, I, Oe0, Oe1, Qz0, Qz1, RQ );
   parameter N = 16;
   
-  input          Clk     ;
-  input          Rst     ;  
-  input          Ld      ; // load signal  
-  input          Oe0     ; // output enable (this is new)  
-  input          Oe1     ;
-  input  [N-1:0] I       ; // data to load
-  output [N-1:0] Qz0, Qz1; // switched output
-  output [N-1:0] RQ      ; // unswitched output
+  input          Clk;
+  input          Rst;  
+  input          Ld ; // load signal  
+  input          Oe0; // output enable (this is new)  
+  input          Oe1;
+  input  [N-1:0] I  ; // data to load
+  output [N-1:0] Qz1; // switched output
+  output [N-1:0] Qz1; // switched output
+  output [N-1:0] RQ ; // unswitched output
   
-  reg    [N-1:0] Q       ;   // standard (unswitched) output
+  reg    [N-1:0] Q  ; // standard (unswitched) output
 
   assign RQ = Q;
 
