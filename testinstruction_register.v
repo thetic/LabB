61e011e7de8e4db79bd0c5030cec0fc0964be8f5
thetic <thetic@autistici.org>
2014-06-06 23:16:53 -0700

    everything

--- testinstruction_register.v
+++ testinstruction_register.v
@@ -3,34 +3,35 @@
 // Ben Foster
 `timescale 1ns / 1 ns
 module testinstruction_register;
-  	reg             clk 	;  	// clock
-  	reg 	[15:0]	d    	;	// input data
-  	reg 			ir_ld 	; 	//load instruction signal
-  	wire	[15:0] q 		;	// output data           
+    reg          clk  ; // clock
+    reg   [15:0] d    ; // input data
+    reg          ir_ld; //load instruction signal
+    wire  [15:0] q    ; // output data
 
-	// Reference module instruction_register #(.N(16)) instruction_register0 (Clock, d, q, ir_ld);
-	//Module under test 
-	instruction_register #(.N(16)) instruction_register0 (clk, d, q, ir_ld);
+  // Reference module instruction_register #(.N(16)) instruction_register0 (Clock, d, q, ir_ld);
+  //Module under test
+  instruction_register #(.N(16)) instruction_register0 (clk, d, q, ir_ld);
 
-	integer I;
+  integer I;
 
-	initial //Clock generator
-		begin
-			Clock = 0;
-			forever #10 Clock = !Clock;
-		end
+  initial //Clock generator
+    begin
+      clk = 0;
+      d = 0;
+      ir_ld = 1;
+      forever #10 clk = !clk;
+    end
 
-	initial //Test stimulus
-		begin
-			ir_ld = 1'b1; //Set ir_ld to 1 in order to test
-			#20 q = 16'hb0b0;
-			#20 ir_ld = 1'b1; //make sure d isn't changed
-			#20 q = 16'hd0d0;
-			#20 $stop;
-		end
+  initial //Test stimulus
+    begin
+      #20 d = 16'hb0b0;
+      #20 ir_ld = 1'b0; //make sure d isn't changed
+      #20 d = 16'hd0d0;
+      #20 $stop;
+    end
 
-	initial
-		$monitor("Time is %d | input = %h | output = %h", $stime, d, q);
+  initial
+    $monitor("Time is %d | input = %h | output = %h", $stime, d, q);
 
 
 
