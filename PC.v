50ea89e908b516d574364fba3a45bfb60b2f524a
thetic <thetic@autistici.org>
2014-06-06 19:19:34 -0700

    modelsim fixes

--- PC.v
+++ PC.v
@@ -7,17 +7,19 @@
 module PC(O, Clear, Up, Clock);
 	input Clear, Up, Clock; // Clear, Increment, Clock
 	output reg [4:0]O;      // PC Output
-	
+
 	localparam zero = 5'b0; // For clearing the PC
-	
+
+    initial O = 0;
+
 	wire [4:0]m0i, mc, I; // Mux 0 Input, Mux 0-1 Connector, PC Input
-	
+
 	PCAdder PCAdder (O, m0i); // Increments the PC
-	
+
 	muxlpm m0 (O, m0i, Up, mc); // Determines whether or not to increment
-	
+
 	muxlpm m1 (mc, zero, Clear, I); // Determines whether or not to clear
-	
+
 	always @(posedge Clock) O = I; // Update PC on posedge Clock
 
 endmodule
--- testPC.v
+++ testPC.v
@@ -9,23 +9,24 @@ module testPC;
 	wire	[4:0]	O 		;   // PC Output
 
 	// Reference module PC(O, Clear, Up, Clock);
-	//Module under test 
+	//Module under test
 	PC PC2014(O, Clear, Up, Clock);
 
 	initial //Clock generator
 		begin
 			Clock = 0;
+			Clear = 0;
 			forever #10 Clock = !Clock;
 		end
 
 	initial //Test stimulus
 		begin
 			Up = 1'b1;
-			#30;
+			#100;
 			Up = 1'b0;
-			#30;
+			#100;
 			Clear = 1'b1;
-			#30 $stop;
+			#100 $stop;
 		end
 
 	initial
